12ee6435c659b5f536ce99220af9bd21abf1966d
Mariano Ponti <nanogp@users.noreply.github.com>
2019-03-26 18:13:27 -0300

    Add files via upload

---
+++ Forms/src/clases/Main.java
@@ -0,0 +1,26 @@
+package clases;
+
+import forms.Form;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author mponti
+ */
+public class Main
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        forms.Form formulario = new Form();
+        formulario.setVisible(true);
+    }
+
+}
